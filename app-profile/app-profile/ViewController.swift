94d5cf20e854be6960f415eae4c970d62861294d
Liam Armitage <liamarmitage@Liams-MBP-3.LA.COM>
2018-10-11 12:00:20 +0100

    initial xcode project

---
+++ app-profile/app-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-profile
+//
+//  Created by Liam Armitage on 11/10/2018.
+//  Copyright © 2018 Bassline Music. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
